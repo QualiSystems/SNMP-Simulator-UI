ef027c310df617944a7f18cb07be1156e3f1d16b
Anthony <anton.p@qualisystems.com>
2019-10-30 16:36:43 +0200

    add recording fixtures

--- quali/settings.py
+++ quali/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_media_fixtures',
     'simulator.apps.SimulatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
